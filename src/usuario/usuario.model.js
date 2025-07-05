884a441bcb0a5a5bf673d2e18595a93968cb5a64
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-07-04 23:40:31 -0600

    Se crea el modelo de usuario

---
+++ src/usuario/usuario.model.js
@@ -0,0 +1,56 @@
+import { Schema, model } from "mongoose"
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String
+        },
+        surname: {
+            type: String
+        },
+        username: {
+            type: String,
+            unique: true
+        },
+        address: {
+            type: {
+                zone: { type: String },  // Ejemplo: "Zona 2"
+                municipality: { type: String }, // Ejemplo: "San Lucas"
+                department: { type: String },  // Ejemplo: "Sacatepéquez"
+            },
+        },//depa muni zona
+        mobilePhone: {
+            type: String
+        },
+        country: {
+            type: String    
+        },
+        email: {
+            type: String,
+            unique: true
+        },
+        password: {
+            type: String
+        },
+        profilePicture: {
+            type: String
+        },      
+        rol: {
+            type: String,
+            uppercase: true,
+            enum: ['ADMIN', 'ALUMNO'],
+            default: 'ALUMNO'
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+)
+
+userSchema.methods.toJSON = function(){
+    const { __v, password, ...user } = this.toObject() 
+    return user
+}
+
+export default model('User', userSchema)
