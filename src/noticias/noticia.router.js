9975f0b858033bdda3ed848df937b156814ff9ae
mpamal-2023046 <mpamal-2023046@kinal.edu.gt>
2025-07-13 22:27:26 -0600

    Se arreglan errores

--- src/noticias/noticia.router.js
+++ src/noticias/noticia.router.js
@@ -1,13 +1,14 @@
 import {Router} from "express";
-
+import multer from "multer";
 import { agregarNoticia, eliminarNoticia, todasLasNoticias, noticiaPorId, actualizarNoticia } from "./noticia.controller.js";
 
 const apiNoticia = Router()
+const upload = multer();
 
 apiNoticia.post("/agregarN", agregarNoticia)
 apiNoticia.get("/obtenerN", todasLasNoticias)
 apiNoticia.get("/obtenerN/:id", noticiaPorId)
-apiNoticia.put("/actualizarN/:id", actualizarNoticia)
+apiNoticia.put("/actualizarN/:id", upload.none(), actualizarNoticia)
 apiNoticia.delete("/eliminarN/:id", eliminarNoticia)
 
 export default apiNoticia
