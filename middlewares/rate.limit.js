6eef5b5eba966dd607dfe034d70b45d59662b621
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-07-14 00:30:32 -0600

    Se actualiza la cantidad de solicitudes que pueden haber

--- middlewares/rate.limit.js
+++ middlewares/rate.limit.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit"
 
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 20,
+    max: 200000000,
     message:{
         message: "Your blocked, wait 15 minutes"
     }
