075ea6035462cdd78747ad3221a5e52cff8c7bdf
jgramajo-2023053 <jgramajo-2023053@kinal.edu.gt>
2025-07-15 00:52:38 -0600

    se agrega nuevo endpoint para llamar usuarios de rol ADMIN y ajuste ligero en el modelo de Reporte

--- src/usuario/usuario.routes.js
+++ src/usuario/usuario.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsuarios } from "./usuario.controller.js";
+import { getUsuarios, getUsuariosAdmins } from "./usuario.controller.js";
 
 const user = Router()
 
 user.get('/users', getUsuarios)
+user.get('/admin', getUsuariosAdmins)
 
 export default user
